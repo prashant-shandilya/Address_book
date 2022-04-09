7008126f12edc44e78562a2a298cab17317c8720
Prashant Shandilya <91403042+vikalp999@users.noreply.github.com>
2022-04-09 15:35:55 +0530

    Create Passkey.js

---
+++ Passkey.js
@@ -0,0 +1,11 @@
+const passkey = [
+    'ruh78uyuio',
+    'ioiujnhb78',
+    'polkiokj89',
+    'kolmnjhbu8',
+    'oi98uijkol'
+];
+
+//These are the passkeys which are purchased by the clients in order to use  our API.
+
+module.exports = passkey;
